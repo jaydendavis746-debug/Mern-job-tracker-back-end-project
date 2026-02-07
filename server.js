6d24f68f5e3e59fe2013bea5a6f456661ae2ded0
ranjith.j <ranjithjacob.work@gmail.com>
2026-02-07 13:44:44 +0000

    sets up and tests JWTs

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const logger = require("morgan");
+const testJwtRouter = require("./controllers/test-jwt");
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(logger("dev"));
 
 // Routes go here
+app.use("/test-jwt", testJwtRouter);
 
 app.listen(3000, () => {
   console.log("The express app is ready!");
